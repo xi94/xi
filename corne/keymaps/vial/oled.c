e796a471e96d318128b7af3ce43a5a1223dcf98b
christian <vampyre935@gmail.com>
2025-10-08 17:57:52 +0200

    fixed oled sample code

--- corne/keymaps/vial/oled.c
+++ corne/keymaps/vial/oled.c
@@ -1,12 +1,17 @@
+#ifdef OLED_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_270; }
+
 bool oled_task_user(void) {
   const bool on_main_display = is_keyboard_master();
 
   if (on_main_display) {
-    oled_write_raw_P(PSTR("hello"), false);
+    oled_write_P(PSTR("hello"), false);
   } else {
-    oled_write_raw_P(PSTR("there"), false);
+    oled_write_P(PSTR("there"), false);
   }
   
   return false;
 }
 
+#endif // OLED_ENABLE
